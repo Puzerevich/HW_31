6c9ec6a271ad943472a3faf0e8dd033003ebef88
Puzerevich <puzerevich@gmail.com>
2023-04-20 22:21:18 +0300

    Create SpringWebApplicationTests.java

---
+++ src/test/java/Hw/SpringWebApplicationTests.java
@@ -0,0 +1,11 @@
+package Hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
